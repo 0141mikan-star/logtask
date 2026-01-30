818073359874305bc0fe6fdb95d2f5b10c5e8eb1
0141mikan-star <yua118118118@gmail.com>
2026-01-30 23:40:35 +0900

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import altair as alt
 from streamlit_calendar import calendar
 
 # ページ設定
-st.set_page_config(page_title="個人タスク管理RPG", layout="wide")
+st.set_page_config(page_title="褒めてくれる勉強時間・タスク管理アプリ", layout="wide")
 
 # --- 日本時間 (JST) の定義 ---
 JST = timezone(timedelta(hours=9))
@@ -36,7 +36,7 @@ if st.session_state["toast_msg"]:
     st.toast(st.session_state["toast_msg"], icon="🆙")
     st.session_state["toast_msg"] = None 
 
-st.title("✅ 褒めてくれるタスク管理 (RPG風)")
+st.title("✅ 褒めてくれる勉強時間・タスク管理アプリ ")
 
 # 称号ガチャのリスト
 GACHA_TITLES = [
@@ -1044,3 +1044,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
